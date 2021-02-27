e5d7577c6b274b22e2b57ca48b5fcc49ae803b16
Andy Irving <mahrev24@gmail.com>
2021-02-27 16:56:31 -0600

    Update problemE.py
    aaaaa

--- problemE.py
+++ problemE.py
@@ -1,6 +1,8 @@
+import sys
+res = ""
 for i in sys.stdin:
 	ab = i.split()
-	S = float(ab[0])
-	R = float(ab[1])
-	V = ((R*(S+0.16))/0.067)**(1/2)
-	print(int(V))
+	R = int(ab[0])
+	S = float(ab[1])
+	V = int((((R*(S+0.16))/0.067))**(1/2))
+	print(V)
