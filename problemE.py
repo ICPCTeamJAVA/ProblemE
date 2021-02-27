6092ec809c6db44af12ab84b28cef9de1ed27a00
Andy Irving <mahrev24@gmail.com>
2021-02-27 14:20:04 -0600

    Update problemE.py
    attempted to add parser for multiple lines?

--- problemE.py
+++ problemE.py
@@ -1,5 +1,8 @@
-x = input().split()
-R = float(x[0])
-S = float(x[1])
-V = ((R*(S+0.16))/0.067)**(1/2)
-print(int(V))
+nlist = input().split("\n")
+print(len(nlist))
+for i in nlist: 
+	x = i.split()
+	R = float(x[0])
+	S = float(x[1])
+	V = ((R*(S+0.16))/0.067)**(1/2)
+	print(int(V))
